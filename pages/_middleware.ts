a6a0cf317d1f9a55ef73c156cb0b53d6cd2af80d
James Barnes <41832886+jambobarnes@users.noreply.github.com>
2022-05-30 19:14:18 +0000

    rewrites

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -19,9 +19,6 @@ export default function middleware(req: NextRequest) {
     });
 
   // Only for demo purposes – remove this if you want to use your root domain as the landing page
-  if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
-    return NextResponse.redirect("https://demo.vercel.pub");
-  }
 
   /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
       You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
@@ -31,8 +28,7 @@ export default function middleware(req: NextRequest) {
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
-          .replace(`.vercel.pub`, "")
-          .replace(`.platformize.vercel.app`, "")
+          .replace(`.platforms-vercel-lemon.vercel.app/`, "")
       : hostname.replace(`.localhost:3000`, "");
 
   if (pathname.startsWith(`/_sites`))
@@ -57,7 +53,7 @@ export default function middleware(req: NextRequest) {
 
     if (
       hostname === "localhost:3000" ||
-      hostname === "platformize.vercel.app"
+      hostname === "platforms-vercel-lemon.vercel.app/"
     ) {
       url.pathname = `/home${pathname}`;
       return NextResponse.rewrite(url);
